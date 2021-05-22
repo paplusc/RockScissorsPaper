0913bb9e553fd3ea50311529a6df40f6b1847401
Pablo Cremades Serra <pablocremadesserra@gmail.com>
2021-05-22 18:35:51 +0200

    Initial commit

---
+++ src/main/java/org/pcremades/App.java
@@ -0,0 +1,13 @@
+package org.pcremades;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
